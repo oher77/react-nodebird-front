46e7dcee90d84205046e8ce36a46f9583c371d99
oher77 <oher77@naver.com>
2020-07-06 22:06:30 +0900

    a

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import 'antd/dist/antd.css';
-import widthReduxSaga from 'next-redux-saga';
+import withReduxSaga from 'next-redux-saga';
 
 import wrapper from '../sotre/configureStore';
 
@@ -21,4 +21,4 @@ App.propTypes = {
 };
 
 // 하이오더컴포넌트(HOC)로 감싸주면 된다. withRedux, withReduxSaga 모두 HOC..
-export default wrapper.withRedux(widthReduxSaga(App));
+export default wrapper.withRedux(withReduxSaga(App));
